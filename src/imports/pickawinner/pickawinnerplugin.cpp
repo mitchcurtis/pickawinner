772ea1844d2c75a341b3dfb159e8ca3e5a232c69
Mitch Curtis <mitch.curtis@qt.io>
2016-10-02 15:21:31 +0200

    Remove stuff that was copied from qqc2

--- src/imports/pickawinner/pickawinnerplugin.cpp
+++ src/imports/pickawinner/pickawinnerplugin.cpp
@@ -1,13 +1,6 @@
 #include <QtQml/qqmlextensionplugin.h>
-#include <QtQml/qqml.h>
-#include <QtCore/qurl.h>
-#include <QtCore/qfile.h>
-#include <QtCore/qcoreapplication.h>
-#include <QtCore/private/qfileselector_p.h>
 
 #include <QtQuickControls2/qquickstyle.h>
-#include <QtQuickControls2/private/qquickstyleplugin_p.h>
-#include <QtQuickControls2/private/qquickstyleselector_p.h>
 
 #include "saturationlightnesspicker_p.h"
 
@@ -19,7 +12,7 @@
 //#endif
 //}
 
-class PickAWinnerPlugin: public QQuickStylePlugin
+class PickAWinnerPlugin : public QQmlExtensionPlugin
 {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID QQmlExtensionInterface_iid)
@@ -30,7 +23,7 @@ public:
     void initializeEngine(QQmlEngine *engine, const char *uri);
 };
 
-PickAWinnerPlugin::PickAWinnerPlugin(QObject *parent) : QQuickStylePlugin(parent)
+PickAWinnerPlugin::PickAWinnerPlugin(QObject *parent) : QQmlExtensionPlugin(parent)
 {
 //    initResources();
 }
@@ -39,14 +32,7 @@ void PickAWinnerPlugin::registerTypes(const char *uri)
 {
     qmlRegisterType<SaturationLightnessPicker>(uri, 1, 0, "SaturationLightnessPickerTemplate");
 
-    QQuickStyleSelector selector;
-    selector.setBaseUrl(typeUrl());
-
-    const QString style = QQuickStyle::name();
-    if (!style.isEmpty())
-        QFileSelectorPrivate::addStatics(QStringList() << style.toLower());
-
-    qmlRegisterType(selector.select(QStringLiteral("SaturationLightnessPicker.qml")), uri, 1, 0, "SaturationLightnessPicker");
+    qmlRegisterType(QStringLiteral("SaturationLightnessPicker.qml"), uri, 1, 0, "SaturationLightnessPicker");
 }
 
 void PickAWinnerPlugin::initializeEngine(QQmlEngine *, const char *)
